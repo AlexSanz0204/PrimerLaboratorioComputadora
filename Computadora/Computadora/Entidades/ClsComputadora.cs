692c005b2934f0c699fdb1b38feb3831fed99c90
Chris <lm075032021@unab.edu.sv>
2021-08-16 14:17:00 -0600

    Update ClsComputadora.cs

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -8,5 +8,24 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
-    }
+        private int memoriaram;
+        private int discoduro;
+        private string placamadre;
+        private string microprocesador;
+        private string tarjetadevideo;
+        private string monitor;
+        private string cpu;
+        private string teclado;
+        private string mouse;
+
+        public int Memoriaram { get => memoriaram; set => memoriaram = value; }
+        public int Discoduro { get => discoduro; set => discoduro = value; }
+        public string Placamadre { get => placamadre; set => placamadre = value; }
+        public string Microprocesador { get => microprocesador; set => microprocesador = value; }
+        public string Tarjetadevideo { get => tarjetadevideo; set => tarjetadevideo = value; }
+        public string Monitor { get => monitor; set => monitor = value; }
+        public string Cpu { get => cpu; set => cpu = value; }
+        public string Teclado { get => teclado; set => teclado = value; }
+        public string Mouse { get => mouse; set => mouse = value; }
+    } /// hola
 }
